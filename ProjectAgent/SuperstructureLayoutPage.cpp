136e4400193fefba82c85f9526e12aeab662e16d
WSDOTbricer <WSDOTbricer>
2015-03-04 00:18:50 +0000

    *** empty log message ***

---
+++ ProjectAgent/SuperstructureLayoutPage.cpp
@@ -0,0 +1,74 @@
+// SuperstructureLayoutPage.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "resource.h"
+#include "SuperstructureLayoutPage.h"
+#include "PierDlg.h"
+
+#include <EAF\EAFDisplayUnits.h>
+#include <MFCTools\CustomDDX.h>
+
+// CSuperstructureLayoutPage dialog
+
+IMPLEMENT_DYNAMIC(CSuperstructureLayoutPage, CPropertyPage)
+
+CSuperstructureLayoutPage::CSuperstructureLayoutPage()
+	: CPropertyPage(CSuperstructureLayoutPage::IDD)
+{
+
+}
+
+CSuperstructureLayoutPage::~CSuperstructureLayoutPage()
+{
+}
+
+void CSuperstructureLayoutPage::DoDataExchange(CDataExchange* pDX)
+{
+	CPropertyPage::DoDataExchange(pDX);
+
+   CComPtr<IBroker> pBroker;
+   EAFGetBroker(&pBroker);
+   GET_IFACE2(pBroker,IEAFDisplayUnits,pDisplayUnits);
+   CPierDlg* pParent = (CPierDlg*)GetParent();
+
+   DDX_CBEnum(pDX,IDC_TRANSVERSE_MEASUREMENT_TYPE,pParent->m_PierData.m_TransverseMeasurementType);
+   DDX_UnitValueAndTag(pDX,IDC_DECK_ELEVATION,IDC_DECK_ELEVATION_UNIT,pParent->m_PierData.m_DeckElevation,pDisplayUnits->GetSpanLengthUnit());
+   DDX_UnitValueAndTag(pDX,IDC_CPO,IDC_CPO_UNIT,pParent->m_PierData.m_CrownPointOffset,pDisplayUnits->GetSpanLengthUnit());
+   DDX_UnitValueAndTag(pDX,IDC_BLO,IDC_BLO_UNIT,pParent->m_PierData.m_BridgeLineOffset,pDisplayUnits->GetSpanLengthUnit());
+   DDX_Text(pDX,IDC_SKEW,pParent->m_PierData.m_strOrientation);
+   DDX_CBIndex(pDX,IDC_BEARING_LINE_COUNT,pParent->m_PierData.m_nBearingLines);
+}
+
+
+BEGIN_MESSAGE_MAP(CSuperstructureLayoutPage, CPropertyPage)
+	ON_COMMAND(ID_HELP, OnHelp)
+END_MESSAGE_MAP()
+
+
+// CSuperstructureLayoutPage message handlers
+BOOL CSuperstructureLayoutPage::OnInitDialog()
+{
+   FillTransverseMeasureComboBox();
+
+   CPropertyPage::OnInitDialog();
+
+   return TRUE;  // return TRUE unless you set the focus to a control
+	              // EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CSuperstructureLayoutPage::OnHelp() 
+{
+#pragma Reminder("UPDATE: Update the help context id")
+   //::HtmlHelp( *this, AfxGetApp()->m_pszHelpFilePath, HH_HELP_CONTEXT, IDH_PIERDETAILS_CONNECTIONS );
+}
+
+void CSuperstructureLayoutPage::FillTransverseMeasureComboBox()
+{
+   CComboBox* pcbTransverseMeasure = (CComboBox*)GetDlgItem(IDC_TRANSVERSE_MEASUREMENT_TYPE);
+   pcbTransverseMeasure->ResetContent();
+   int idx = pcbTransverseMeasure->AddString(_T("normal to the alignment"));
+   pcbTransverseMeasure->SetItemData(idx,(DWORD_PTR)xbrTypes::tdmNormalToAlignment);
+   idx = pcbTransverseMeasure->AddString(_T("in the plane of the pier"));
+   pcbTransverseMeasure->SetItemData(idx,(DWORD_PTR)xbrTypes::tdmPlaneOfPier);
+}
---
+++ ProjectAgent/SuperstructureLayoutPage.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+// CSuperstructureLayoutPage dialog
+
+class CSuperstructureLayoutPage : public CPropertyPage
+{
+	DECLARE_DYNAMIC(CSuperstructureLayoutPage)
+
+public:
+	CSuperstructureLayoutPage();
+	virtual ~CSuperstructureLayoutPage();
+
+// Dialog Data
+	enum { IDD = IDD_SUPERSTRUCTURE_LAYOUT_PAGE };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	//{{AFX_MSG(CPierLayoutPage)
+	virtual BOOL OnInitDialog();
+   afx_msg void OnHelp();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+
+   void FillTransverseMeasureComboBox();
+};
