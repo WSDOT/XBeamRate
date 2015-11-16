026472fbff28eefecdf053a4cc82348df9a51bed
WSDOTbricer <WSDOTbricer>
2015-11-16 22:20:15 +0000

    *** empty log message ***

--- UpdateBinFiles.bat
+++ UpdateBinFiles.bat
@@ -6,7 +6,7 @@ SET REGFREECOM=\ARP\BridgeLink\RegFreeCOM
 
 xcopy /Y /d %REGFREECOM%\Win32\Release\XBeamRate*.dll		%BINTARGET%\Win32\
 xcopy /Y /d %REGFREECOM%\x64\Release\XBeamRate*.dll		%BINTARGET%\x64\
-xcopy /Y /d \ARP\BridgeLink\XBeamRate.chm	              	%BINTARGET%\
+xcopy /Y /d \ARP\BridgeLink\XBRate.chm	                	%BINTARGET%\
 
 xcopy /Y /d Images\*.png					%BINTARGET%\Images\
 xcopy /Y /d Images\*.gif					%BINTARGET%\Images\
