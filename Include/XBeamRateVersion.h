6e54fee509e65f69332b19366a543ea1988f4da8
WSDOTbricer <WSDOTbricer>
2018-05-18 20:04:07 +0000

    Updated version numbers

--- Include/XBeamRateVersion.h
+++ Include/XBeamRateVersion.h
@@ -23,7 +23,7 @@
 #pragma once
 
 #ifndef _XBEAMRATE_VERSION
-#define _XBEAMRATE_VERSION 122 // version 1.2.2
+#define _XBEAMRATE_VERSION 123 // version 1.2.3
 #endif
 
 #include <\ARP\BridgeLink\Include\BridgeLinkVersion.h>
