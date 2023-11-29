ec4151ba7479620508d2ef1f3c7c74fb547fa581
Richard Brice <37087370+RickBrice@users.noreply.github.com>
2023-11-29 12:52:52 -0800

    Mantis 667 - More fixes related to build-release process

--- Documentation/BuildDocumentation.bat
+++ Documentation/BuildDocumentation.bat
@@ -18,6 +18,6 @@ echo ^</script^> >> %SEARCH_HTML%
 %ARPDIR%\BridgeLink\RegFreeCOM\x64\Release\MakeDocMap XBRate
 rmdir /S /Q %ARPDIR%\BridgeLink\Docs\XBRate\%1\
 mkdir %ARPDIR%\BridgeLink\Docs\XBRate\%1\
-copy %ARPDIR%\XBeamRate\Documentation\doc\html\* %ARPDIR%\BridgeLink\Docs\XBRate\%1\
+xcopy /s /y /d %ARPDIR%\XBeamRate\Documentation\doc\html\* %ARPDIR%\BridgeLink\Docs\XBRate\%1\
 copy %ARPDIR%\XBeamRate\Documentation\XBRate.dm %ARPDIR%\BridgeLink\Docs\XBRate\%1\
 
